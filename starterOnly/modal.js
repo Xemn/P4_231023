1af4e844cf15c5a6f1eb492848ccef5160e6087f
Xemn <spysschaert.pro@gmail.com>
2023-10-24 16:12:14 +0200

    Create a closeModal funtion and apply it to an eventListener link to the close button

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,25 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+
+console.log(closeBtn);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
+// close modal form
+function closeModal ()
+{
+  if (modalbg.style.display === "block")
+  {
+    modalbg.style.display = "none";
+  }
+}
 
