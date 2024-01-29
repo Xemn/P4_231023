98e6b89b99b445c1726531c0cd7ff74ea1f7507d
Xemn <spysschaert.pro@gmail.com>
2024-01-29 17:38:06 +0100

    Adding commentary for each funtions - Change the throw logic for each function - Creating a function who display the error Message - Creating a function who display a reservation modal - Adding a deleting logic of existing erros in my validate function and adding the use of closeModal, resetForm and reservationModal function

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,83 +1,163 @@
+// Fonction qui vérifie si le nom entré est correcte : 
 function validateName (field)
 {
     if (field.value.length < 2 || field.value === null)
     {
-        throw new Error (`Votre ${field.id}name est trop court.`);
+        throw {
+            id : field.id,
+            error : new Error ("Ce champ doit possèder au minimum deux caractères") 
+            
+        }
+        //new Error ("Ce champ doit possèder au minimum deux caractères");
     }
 
 }
 
+// Fonction qui vérifie si l'email entré est correcte : 
 function validateEmail (email)
 {
     let regexMail = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
 
     if (!regexMail.test(email.value))
     {
-        throw new Error ("Ceci n'est pas une adresse mail valide.");
+        throw {
+            id : email.id,
+            error :new Error ("Ceci n'est pas une adresse mail valide.")
+        }
     }
 }
 
+// Fonction qui vérifie si la nombre entré est correcte : 
 function validateNumberTurnament (numberTurnament)
 {
     let regexNumber = new RegExp("^[0-9]+$");
 
     if (!regexNumber.test(numberTurnament.value))
     {
-        throw new Error ("Ceci n'est pas une valeur correcte.");
+        throw {
+            id : numberTurnament.id,
+            error : new Error ("Ceci n'est pas une valeur correcte.")
+        }
     }
 }
 
+// Fonction qui vérifie si une ville a été séléctionnée : 
 function getCityChoice ()
 {
     const location = document.querySelector("form").location.value;
 
     if (location === "")
     {
-        throw new Error ("Une destination doit être séléctionée.");
+        throw {
+            id : "quantity",
+            error : new Error ("Vous devez séléctionner une ville.")
+        }
     }
 }
 
+// Fonction qui vérfie que la date entrée est valide : 
 function validateDate (date)
 {
-    let regexDate = new RegExp("^((19|20)?[0-9]{2}[- .](0?[1-9]|1[012])[- .](0?[1-9]|[12][0-9]|3[01]))*$")
-
+    let regexDate = new RegExp("^((19|20)?[0-9]{2}[- .](0?[1-9]|1[012])[- .](0?[1-9]|[12][0-9]|3[01]))*$");
+    
     if(date.value = "" || !regexDate.test(date.value))
     {
-        throw new Error ("Votre date de naissance est incorrecte.");
+        throw {
+            id : date.id ,
+            error: new Error ("Votre date de naissance est incorrecte.")
+        }
     }
 }
 
+// Fonction qui vérifié si les CGU ont été cochées : 
 function validateCGU ()
 {
     const CGU = document.getElementById("checkbox1");
 
     if (!CGU.checked)
     {
-        throw new Error ("Vous devez accepter les CGU.");
+        throw {
+            id : "checkbox1" ,
+            error : new Error ("Vous devez accepter les CGU.")
+        }
     }
+}
+
+// Fonction qui affiche un message d'erreur su le champ est mal renseigné : 
+function displayErrorMessage(fieldId, errorMessage) {
+    let spanErrorMessage = document.getElementById(`${fieldId}-error`);
+
+    if (!spanErrorMessage) {
+        let input = document.getElementById(fieldId);
+        spanErrorMessage = document.createElement("span");
+        spanErrorMessage.id = `${fieldId}-error`;
+        spanErrorMessage.innerHTML = errorMessage;
+        input.after(spanErrorMessage);
+    }
+}
+
+// Fonction qui affiche un message de confirmation : 
+function reservationModal() {
+    const modal = document.createElement("div");
+    modal.innerHTML = `
+    <div class="modal-body">
+        <div class="bground validation">
+            <div class="content">
+                <span class="close closeValidation"></span>
+                <div class="modal-body">
+                    <p>Votre résérvation a été prise en compte ! </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    document.body.appendChild(modal);
+}
+
+
+// fonction qui réinitialise le formulaire : 
+function resetForm ()
+{
+    const form = document.querySelector("form");
+    form.reset();
 }
 
 function validate (event)
 {
+    // Empêche le comportement par défaut (envoie du formulaire)
     event.preventDefault();
     
+    /* Toutes les constantes récupérants les différents champs dont
+    nous avons besoin : */
     const firstName = document.getElementById("first");
     const lastName = document.getElementById("last");
     const email = document.getElementById("email");
     const date = document.getElementById("birthdate");
     const numberTurnament = document.getElementById("quantity");
-    
+   
+    // Supprimez tous les messages d'erreur existants
+    const errorElements = document.querySelectorAll("[id$='-error']");
+    errorElements.forEach(element => element.remove());
 
+    // "Essaie" chacune des fonctions : 
     try {
         validateName(firstName);
         validateName(lastName);
         validateEmail(email);
         validateNumberTurnament(numberTurnament);
-        validateDate(date)
+        validateDate(date);
         getCityChoice();
         validateCGU();
+
+        closeModal();
+        resetForm();
+
+        reservationModal();
+
         
-    } catch (error) {
-        console.log(error.message);
+    }
+    // Si l'essaie échoue :  
+    catch ({id, error}) {
+        displayErrorMessage(id, error.message)
     }
 }
